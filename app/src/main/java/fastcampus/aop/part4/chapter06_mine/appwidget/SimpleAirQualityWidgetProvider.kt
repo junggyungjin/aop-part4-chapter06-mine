5ade7a84998270ac63234f19adaedd116edf7ca9
junggyungjin <junggyungjin@svcorps.dev>
2022-07-26 10:49:46 +0900

    project complete

--- app/src/main/java/fastcampus/aop/part4/chapter06_mine/appwidget/SimpleAirQualityWidgetProvider.kt
+++ app/src/main/java/fastcampus/aop/part4/chapter06_mine/appwidget/SimpleAirQualityWidgetProvider.kt
@@ -1,13 +1,29 @@
 package fastcampus.aop.part4.chapter06_mine.appwidget
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
-import android.util.Log
+import android.view.View
+import android.widget.RemoteViews
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.lifecycleScope
+import com.google.android.gms.location.LocationServices
+import fastcampus.aop.part4.chapter06_mine.R
+import fastcampus.aop.part4.chapter06_mine.data.Repository
+import fastcampus.aop.part4.chapter06_mine.data.models.airquality.Grade
+import kotlinx.coroutines.launch
 
 class SimpleAirQualityWidgetProvider : AppWidgetProvider() {
 
@@ -18,16 +34,86 @@ class SimpleAirQualityWidgetProvider : AppWidgetProvider() {
     ) {
         super.onUpdate(context, appWidgetManager, appWidgetIds)
 
+        ContextCompat.startForegroundService(
+            context!!,
+            Intent(context, UpdateWidgetService::class.java)
+        )
+
     }
 
     class UpdateWidgetService : LifecycleService() {
         override fun onCreate() {
             super.onCreate()
 
+            createChannelIfNeeded()
+            startForeground(
+                NOTIFICATION_ID,
+                createNotification()
+            )
         }
 
-        fun createChannelIfNeeded() {
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        @SuppressLint("RemoteViewLayout")
+        override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                val updateViews = RemoteViews(packageName, R.layout.widget_simple).apply {
+                    setTextViewText(
+                        R.id.resultTextView,
+                        "권한 없음"
+                    )
+                    setViewVisibility(R.id.labelTextView, View.GONE)
+                    setViewVisibility(R.id.gradeLabelTextView, View.GONE)
+                }
+                updateWidget(updateViews)
+                stopSelf()
+
+                return super.onStartCommand(intent, flags, startId)
+            }
+            LocationServices.getFusedLocationProviderClient(this).lastLocation
+                .addOnSuccessListener { location ->
+                    lifecycleScope.launch {
+                        try {
+                            val nearbyMonitoringStation = Repository.getNearbyMonitoringStation(
+                                location.latitude,
+                                location.longitude
+                            )
+                            val measuredValue =
+                                Repository.getLatestAirQualityData(nearbyMonitoringStation!!.stationName!!)
+                            val updateViews =
+                                RemoteViews(packageName, R.layout.widget_simple).apply {
+                                    setViewVisibility(R.id.labelTextView, View.VISIBLE)
+                                    setViewVisibility(R.id.gradeLabelTextView, View.VISIBLE)
+
+                                    val currentGrade = (measuredValue?.khaiGrade ?: Grade.UNKNOWN)
+                                    setTextViewText(R.id.resultTextView, currentGrade.emoji)
+                                    setTextViewText(R.id.gradeLabelTextView, currentGrade.label)
+                                }
+
+                            updateWidget(updateViews)
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        } finally {
+                            stopSelf()
+                        }
+
+                    }
+                }
+
+            return super.onStartCommand(intent, flags, startId)
+        }
+
+        override fun onDestroy() {
+            super.onDestroy()
+
+            stopForeground(true)
+        }
+
+        private fun createChannelIfNeeded() {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 (getSystemService(NOTIFICATION_SERVICE) as? NotificationManager)
                     ?.createNotificationChannel(
                         NotificationChannel(
@@ -38,10 +124,23 @@ class SimpleAirQualityWidgetProvider : AppWidgetProvider() {
                     )
             }
         }
+
+        private fun createNotification(): Notification =
+            NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_baseline_refresh_24)
+                .setChannelId(WIDGET_REFRESH_CHANNEL_ID)
+                .build()
+
+        private fun updateWidget(updateViews: RemoteViews) {
+            val widgetProvider = ComponentName(this, SimpleAirQualityWidgetProvider::class.java)
+            AppWidgetManager.getInstance(this).updateAppWidget(widgetProvider, updateViews)
+        }
+
     }
 
     companion object {
         private const val WIDGET_REFRESH_CHANNEL_ID = "WIDGET_REFRESH_CHANNEL_ID"
+        private const val NOTIFICATION_ID = 101
     }
 
 }
